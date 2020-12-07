f3240b275431ff6251a32297cfa97b29d0ccf971
Ben Tasker <RC0043640@student.riversidecollege.ac.uk>
2020-12-07 15:04:11 +0000

    Changed text in the text boxes to "Hello Tom" and "Hello Bruce"

--- Hello World1/Form1.Designer.cs
+++ Hello World1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtBxName = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -44,17 +45,28 @@
             // 
             // txtBxName
             // 
-            this.txtBxName.Font = new System.Drawing.Font("Agency FB", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxName.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBxName.Location = new System.Drawing.Point(602, 103);
             this.txtBxName.Name = "txtBxName";
-            this.txtBxName.Size = new System.Drawing.Size(100, 27);
+            this.txtBxName.Size = new System.Drawing.Size(100, 26);
             this.txtBxName.TabIndex = 1;
+            this.txtBxName.Text = "Hello Tom";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(317, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Hello Bruce";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtBxName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
